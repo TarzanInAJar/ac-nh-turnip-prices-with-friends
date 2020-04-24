028fe929dd892c43a7d17e498ddead3bbf60e7c0
Ryan Nichols <rybread@gmail.com>
2020-04-23 21:50:07 -0400

    making this just a repo for now

--- ac-nh-turnip-prices-with-friends/src/main/java/com/mytechuncle/acnh/repositories/TurnipWeekRepository.java
+++ ac-nh-turnip-prices-with-friends/src/main/java/com/mytechuncle/acnh/repositories/TurnipWeekRepository.java
@@ -3,7 +3,8 @@ package com.mytechuncle.acnh.repositories;
 import com.mytechuncle.acnh.models.TurnipWeek;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
+@Repository
 public interface TurnipWeekRepository extends JpaRepository<TurnipWeek, Long> {
 }
