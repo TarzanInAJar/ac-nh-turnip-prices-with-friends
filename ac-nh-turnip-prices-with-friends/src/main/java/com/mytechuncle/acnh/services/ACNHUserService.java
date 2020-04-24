bb1554a6a54244c99b072b4ec03e3e2ca897442a
Ryan Nichols <rybread@gmail.com>
2020-04-24 00:06:59 -0400

    userservice now has getTurnipWeek method

--- ac-nh-turnip-prices-with-friends/src/main/java/com/mytechuncle/acnh/services/ACNHUserService.java
+++ ac-nh-turnip-prices-with-friends/src/main/java/com/mytechuncle/acnh/services/ACNHUserService.java
@@ -1,7 +1,9 @@
 package com.mytechuncle.acnh.services;
 
 import com.mytechuncle.acnh.models.ACNHUser;
+import com.mytechuncle.acnh.models.TurnipWeek;
 import com.mytechuncle.acnh.repositories.ACNHUserRepository;
+import com.mytechuncle.acnh.repositories.TurnipWeekRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class ACNHUserService {
     @Autowired
     ACNHUserRepository acnhUserRepository;
 
+    @Autowired
+    TurnipWeekRepository turnipWeekRepository;
+
     public ACNHUser getUserFromOAuth(OAuth2User user) {
         String email = user.getAttribute("email");
         Optional<ACNHUser> optionalUser = acnhUserRepository.findUserByEmail(email);
@@ -23,5 +28,17 @@ public class ACNHUserService {
             return acnhUserRepository.save(acnhUser);
         }
         return optionalUser.get();
+    }
+
+    public TurnipWeek getTurnipWeek(ACNHUser user, int year, int week) {
+        Optional<TurnipWeek> optionalTurnipWeek = turnipWeekRepository.findByUserAndYearAndWeek(user, year, week);
+        if (!optionalTurnipWeek.isPresent()) {
+            TurnipWeek turnipWeek = new TurnipWeek();
+            turnipWeek.setUser(user);
+            turnipWeek.setYear(year);
+            turnipWeek.setWeek(week);
+            return turnipWeekRepository.save(turnipWeek);
+        }
+        return optionalTurnipWeek.get();
     }
 }
