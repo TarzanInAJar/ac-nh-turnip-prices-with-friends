dcab8f949a90b7bca79fd1841b4bba53ea4048b9
Ryan Nichols <rybread@gmail.com>
2020-04-24 21:29:09 -0400

    setting security configuration

--- ac-nh-turnip-prices-with-friends/src/main/java/com/mytechuncle/acnh/TurnipPricesWithFriendsApplication.java
+++ ac-nh-turnip-prices-with-friends/src/main/java/com/mytechuncle/acnh/TurnipPricesWithFriendsApplication.java
@@ -2,12 +2,35 @@ package com.mytechuncle.acnh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
-public class TurnipPricesWithFriendsApplication {
+public class TurnipPricesWithFriendsApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(TurnipPricesWithFriendsApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+        // @formatter:on
     }
 
 }
