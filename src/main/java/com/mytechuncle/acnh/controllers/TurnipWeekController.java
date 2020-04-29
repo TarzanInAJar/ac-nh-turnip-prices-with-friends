6c4fd59b319e567fd8bf2ec025e61179aeac805f
Ryan Nichols <rybread@gmail.com>
2020-04-29 13:02:37 -0400

    adding a temp method to make an all encompassing user group, getting a group by id

--- src/main/java/com/mytechuncle/acnh/controllers/TurnipWeekController.java
+++ src/main/java/com/mytechuncle/acnh/controllers/TurnipWeekController.java
@@ -1,26 +1,29 @@
 package com.mytechuncle.acnh.controllers;
 
 import com.mytechuncle.acnh.models.ACNHUser;
+import com.mytechuncle.acnh.models.TurnipUserGroup;
 import com.mytechuncle.acnh.models.TurnipWeek;
 import com.mytechuncle.acnh.models.ui.TurnipWeekLocalStorage;
+import com.mytechuncle.acnh.repositories.ACNHUserRepository;
+import com.mytechuncle.acnh.repositories.TurnipUserGroupRepository;
 import com.mytechuncle.acnh.services.ACNHUserService;
 import com.mytechuncle.acnh.services.TurnipDTOService;
+import com.mytechuncle.acnh.services.TurnipUserGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.temporal.TemporalField;
 import java.time.temporal.WeekFields;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/turnips")
@@ -32,6 +35,15 @@ public class TurnipWeekController {
     @Autowired
     TurnipDTOService turnipService;
 
+    @Autowired
+    TurnipUserGroupService groupService;
+
+    @Autowired
+    ACNHUserRepository userRepository;
+
+    @Autowired
+    TurnipUserGroupRepository turnipUserGroupRepository;
+
     @GetMapping("")
     public ResponseEntity<TurnipWeek> getCurrentTurnipWeek(@AuthenticationPrincipal OAuth2User user) {
         ACNHUser acnhUser = userService.getUserFromOAuth(user);
@@ -40,6 +52,35 @@ public class TurnipWeekController {
         int weekNumber = localDate.get(woy);
         TurnipWeek turnipWeek = userService.getTurnipWeek(acnhUser, localDate.getYear(), weekNumber);
         return new ResponseEntity<>(turnipWeek, HttpStatus.OK);
+    }
+
+    @GetMapping("/{groupid}")
+    public ResponseEntity<List<TurnipWeek>> getCurrentGroupTurnipWeeks(@AuthenticationPrincipal OAuth2User user, @PathVariable("groupid") Long groupId) {
+        ACNHUser acnhUser = userService.getUserFromOAuth(user);
+        LocalDate localDate = LocalDate.now();
+        TemporalField woy = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear();
+        int weekNumber = localDate.get(woy);
+        Optional<TurnipUserGroup> optionalGroup = turnipUserGroupRepository.findById(groupId);
+        if (!optionalGroup.isPresent()) {
+            // TODO Handle
+        }
+        TurnipUserGroup group = optionalGroup.get();
+        if (group.getMembers().contains(acnhUser)) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        List<TurnipWeek> groupWeeks = userService.getGroupTurnipWeeks(group, localDate.getYear(), weekNumber);
+        return new ResponseEntity<>(groupWeeks, HttpStatus.OK);
+    }
+
+    @GetMapping("/deleteme")
+    public ResponseEntity<Void> addAllToTempGroup(@AuthenticationPrincipal OAuth2User user) {
+        ACNHUser acnhUser = userService.getUserFromOAuth(user);
+        List<ACNHUser> allUsers = userRepository.findAll();
+        TurnipUserGroup group = groupService.createOrReturnGroup("Pooski", acnhUser);
+        group.setMembers(allUsers);
+        turnipUserGroupRepository.save(group);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("")
